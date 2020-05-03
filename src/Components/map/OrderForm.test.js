c1f4632534bdaa21fb38ce255f760beb160eb477
Oleg V. Anisov <anisov@jet.su>
2020-05-03 16:40:00 +0300

    test(components): added test to order form

--- src/Components/map/OrderForm.test.js
+++ src/Components/map/OrderForm.test.js
@@ -4,11 +4,30 @@ import OrderForm from './OrderForm';
 import { renderWithProviders } from '../../helpers/testUtils';
 
 describe('Order form', () => {
-    it('get text element', () => {
+    const orderTaxiMock = jest.fn();
+
+    it('get elements by text', () => {
         const { getByText } = renderWithProviders(
-            <OrderForm orderTaxi={jest.fn()} />
+            <OrderForm orderTaxi={orderTaxiMock} />
         );
 
         wait(() => expect(getByText('Откуда')).toBeInTheDocument());
+        wait(() => expect(getByText('Куда')).toBeInTheDocument());
+        wait(() => expect(getByText('Вызвать такси')).toBeInTheDocument());
+    });
+
+    it('click button', () => {
+        const { getByText } = renderWithProviders(
+            <OrderForm orderTaxi={orderTaxiMock} />
+        );
+
+        fireEvent.click(getByText('Вызвать такси'), {
+            target: {
+                address_from: 'Адрес1',
+                address_to: 'Адрес2',
+            },
+        });
+        wait(() => expect(orderTaxiMock).toHaveBeenCalled());
+        wait(() => expect(/заказ размещён/i).toBeInTheDocument());
     });
 });
